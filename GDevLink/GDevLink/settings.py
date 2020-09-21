61d7c5c94a2a88907838c4c7c87767f181ffd58d
JuanCarlosSuarezJaimes <juan.suarezj@javeriana.edu.co>
2020-09-21 14:32:19 -0500

    Configuración de base dedatos Postgres y creación de modelo Usuario.

--- GDevLink/GDevLink/settings.py
+++ GDevLink/GDevLink/settings.py
@@ -76,11 +76,16 @@ WSGI_APPLICATION = 'GDevLink.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gdevlink',
+        'USER': 'deltasoft',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
+AUTH_USER_MODEL = 'gdl.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
--- GDevLink/gdl/models.py
+++ GDevLink/gdl/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
 
-# Create your models here.
+class User(AbstractUser):
+    PosiblesRoles = models.TextChoices('PosiblesRoles', 'Programador Director Artista Productor')
+    PosiblesGeneros = models.TextChoices('PosiblesGeneros','Plataforma Pelea Shooter RPG')
+    PosiblesFrameworks = models.TextChoices('PosiblesFrameworks','Unity UnrealEngine')
+    
+    roles = ArrayField(models.CharField(blank=False, choices=PosiblesRoles.choices, max_length=20))
+    generos = ArrayField(models.CharField(blank=False, choices=PosiblesGeneros.choices, max_length=20))
+    frameworks = ArrayField(models.CharField(blank=False, choices=PosiblesFrameworks.choices, max_length=20))
+    descripcion = models.CharField(max_length=500, blank=True)
