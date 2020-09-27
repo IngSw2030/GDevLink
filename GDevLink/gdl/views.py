6f782eeb184e17bfcaff0599c5132388fbad37e5
Reshi01 <danieldanih97@gmail.com>
2020-09-27 12:24:58 -0500

    Inicio Sesion

--- GDevLink/gdl/views.py
+++ GDevLink/gdl/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login, logout
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,4 +7,21 @@ def index(request):
     return HttpResponse("Hello, world!")
 
 def login(request):
+    if request.method == "POST":
+            username=request.POST["username"]
+            password=request.POST["password"]
+            user=authenticate(request,username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return("index")
+            else:
+                return render(request, "login",{
+                    "message": "Datos de inicio de sesion incorrecots"
+                })
     return HttpResponse("Login")
+
+def logout(request):
+    logout(request)
+    return render(request,"login",{
+        "message": "Sesion cerrada"
+    })
