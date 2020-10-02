fff2a450804b34e2c1ae9ea9ca0d5d4d9dfeeec5
JuanCarlosSuarezJaimes <juan.suarezj@javeriana.edu.co>
2020-10-02 15:37:58 -0500

    Modelos de app proyecto creados. Creación de enumerados para las opciones de los campos.

--- GDevLink/proyectos/models.py
+++ GDevLink/proyectos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from usuarios.models import Usuario
+from main.enum import PosiblesFases, PosiblesPermisos, PosiblesRoles, PosiblesGeneros, PosiblesFrameworks
 
-# Create your models here.
+class Proyecto(models.Model):
+    nombre = models.CharField(max_length=50, blank=False)
+    generos = ArrayField(models.CharField(blank=False, choices=PosiblesGeneros.choices, max_length=2))
+    fase = models.CharField(blank=False, choices=PosiblesFases.choices, max_length=2)
+    descripcion = models.CharField(max_length=500, blank=True)
+    frameworks = ArrayField(models.CharField(blank=False, choices=PosiblesFrameworks.choices, max_length=2))
+    #imagenes
+    enlace_video = models.CharField(max_length=500, blank=True)
+    enlace_juego = models.CharField(max_length=500, blank=True)
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+
+class Participacion(models.Model):
+    usuario = models.ForeignKey('usuarios.Usuario',on_delete=models.CASCADE,related_name='participaciones')
+    proyecto = models.ForeignKey('Proyecto',on_delete=models.CASCADE,related_name='participaciones')
+    roles = ArrayField(models.CharField(blank=False, choices=PosiblesRoles.choices, max_length=2))
+    permiso = models.CharField(blank=False, choices=PosiblesPermisos.choices, max_length=2)
+
+    
--- GDevLink/usuarios/models.py
+++ GDevLink/usuarios/models.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.contrib.postgres.fields import ArrayField
+from main.enum import PosiblesFrameworks, PosiblesGeneros, PosiblesRoles
 
-class User(AbstractUser):
-    PosiblesRoles = models.TextChoices('PosiblesRoles', 'Programador Director Artista Productor')
-    PosiblesGeneros = models.TextChoices('PosiblesGeneros','Plataforma Pelea Shooter RPG')
-    PosiblesFrameworks = models.TextChoices('PosiblesFrameworks','Unity UnrealEngine')
-    
-    roles = ArrayField(models.CharField(blank=False, choices=PosiblesRoles.choices, max_length=20))
-    generos = ArrayField(models.CharField(blank=False, choices=PosiblesGeneros.choices, max_length=20))
-    frameworks = ArrayField(models.CharField(blank=False, choices=PosiblesFrameworks.choices, max_length=20))
+class Usuario(AbstractUser): 
+    roles = ArrayField(models.CharField(blank=False, choices=PosiblesRoles.choices, max_length=2))
+    generos = ArrayField(models.CharField(blank=False, choices=PosiblesGeneros.choices, max_length=2))
+    frameworks = ArrayField(models.CharField(blank=False, choices=PosiblesFrameworks.choices, max_length=2))
     descripcion = models.CharField(max_length=500, blank=True)
+    #imagen
