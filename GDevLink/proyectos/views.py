21caadbbb701d4c1f4411bc1c4078db5ec715dba
Reshi01 <danieldanih97@gmail.com>
2020-10-12 12:20:57 -0500

    Arreglo Crear Proyecto

--- GDevLink/proyectos/views.py
+++ GDevLink/proyectos/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from main.enum import PosiblesFrameworks, PosiblesGeneros, PosiblesFases
+from main.enum import PosiblesFrameworks, PosiblesGeneros, PosiblesFases,PosiblesPermisos
+from proyectos.models import Proyecto, Participacion
+from datetime import datetime
 # Create your views here.
 
 def proyectosUsuario(request):
@@ -12,22 +14,34 @@ def proyectosUsuario(request):
 
 def crearProyecto(request):
     if request.method == "POST" and 'crearProyecto' in request.POST:
+        username=request.POST["username"]
+        password=request.POST["password"]
+        usuario=authenticate(request,username=username, password=password)
         nombre = request.POST["name"]
         generos = request.POST.getlist("generos")
         fase = request.POST.getlist('fase')
         descripcion = request.POST['descripcion']
         frameworks = request.POST.getlist('frameworks')
+        enlace_video =request.POST["enlaceVideo"] 
         if(nombre == ""):
-            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros, "message": "Por favor ingresar un nombre"})
+            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros,
+             "message": "Por favor ingresar un nombre"})
         if(len(fase)!=1):
-            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros, "message": "Seleccione una (1) fase"})
+            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros,
+             "message": "Seleccione una (1) fase"})
         if(len(frameworks)==0):
-            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros, "message": "Seleccione al menos un (1)framework"})
-        #imagen = 
-        #galeria = 
-        #enlace_video = 
-        #enlace_video = 
+            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros,
+             "message": "Seleccione al menos un (1) framework"})
+        imagenes = request.POST.getlist('imagenes')        
         #enlace_juego = 
-        #fecha_creacion
-    
+        try:
+            proyecto = Proyecto(nombre=nombre,generos=generos,fase=fase,descripcion=descripcion,frameworks=frameworks,galeria=imagenes,enlace_video=enlace_video)
+            proyecto.save()
+
+            participacion= Participacion(usuario=usuario,proyecto=proyecto,,permiso=PosiblesPermisos.MASTER)
+            participacion.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "proyectos/crearProyecto.html", {
+                "message": "Nombre de proyecto ya registrado"})
     return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros})
