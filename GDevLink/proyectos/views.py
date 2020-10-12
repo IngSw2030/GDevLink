404d283ef5f93b5cf625bb5a2eddcd8b3b5c0a84
Reshi01 <danieldanih97@gmail.com>
2020-10-12 09:54:26 -0500

    Actualizacion

--- GDevLink/proyectos/views.py
+++ GDevLink/proyectos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from main.enum import PosiblesFrameworks, PosiblesGeneros, PosiblesFases
 # Create your views here.
 
 def proyectosUsuario(request):
@@ -13,14 +13,21 @@ def proyectosUsuario(request):
 def crearProyecto(request):
     if request.method == "POST" and 'crearProyecto' in request.POST:
         nombre = request.POST["name"]
-        #generos = request.POST.getlist["generos"]
+        generos = request.POST.getlist("generos")
         fase = request.POST.getlist('fase')
-        descripcion = request.POST('descripcion')
+        descripcion = request.POST['descripcion']
         frameworks = request.POST.getlist('frameworks')
+        if(nombre == ""):
+            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros, "message": "Por favor ingresar un nombre"})
+        if(len(fase)!=1):
+            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros, "message": "Seleccione una (1) fase"})
+        if(len(frameworks)==0):
+            return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros, "message": "Seleccione al menos un (1)framework"})
         #imagen = 
         #galeria = 
         #enlace_video = 
         #enlace_video = 
         #enlace_juego = 
         #fecha_creacion
-    return render(request,"proyectos/crearProyecto.html")
+    
+    return render(request,"proyectos/crearProyecto.html",{"generos":PosiblesGeneros ,"fases":PosiblesFases ,"frameworks":PosiblesGeneros})
