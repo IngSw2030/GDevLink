2577e7d2af4e055f0ad84dfc29a4aa72d678b8a4
SantiagoCaroprese <66383253+SantiagoCaroprese@users.noreply.github.com>
2020-10-14 11:49:45 -0500

    IndexC

--- GDevLink/main/urls.py
+++ GDevLink/main/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path("layout", views.layout, name="layout"),
     path("", views.index, name="index"),
     path('login', include(('usuarios.urls', 'vista_login'), namespace='usuarios')),
+    path('crearProyecto', include(('proyectos.urls', 'crearProyecto'), namespace='proyectos')),
     path('logout', include(('usuarios.urls', 'logout'), namespace='usuarios'))
 ]
