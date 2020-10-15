894c9e6acfbd853c6b2ed34e5c7eb62a0ec0ce30
Reshi01 <danieldanih97@gmail.com>
2020-10-15 18:10:02 -0500

    Arreglo al logout

--- GDevLink/usuarios/urls.py
+++ GDevLink/usuarios/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("login", views.vista_login, name="login"),
-    path("logout", views.logout, name="logout"),
+    path("logout", views.vista_logout, name="logout"),
     path("registrar", views.registrar, name="registrar"),
     path("perfil", views.perfil, name="perfil")
 ]
