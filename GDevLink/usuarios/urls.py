2d8eb9c833f6449594495ce1c7795bd5be8544c9
JuanCarlosSuarezJaimes <juan.suarezj@javeriana.edu.co>
2020-10-16 00:16:38 -0500

    Se terminó back-end de página de proyecto y de perfil. Se arregló contenido de front-end de página de proyecto y perfil.

--- GDevLink/usuarios/urls.py
+++ GDevLink/usuarios/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path("login", views.vista_login, name="login"),
     path("logout", views.vista_logout, name="logout"),
     path("registrar", views.registrar, name="registrar"),
-    path("perfil", views.perfil, name="perfil")
+    path("perfil/<str:nombre_usuario>", views.perfil, name="perfil")
 ]
