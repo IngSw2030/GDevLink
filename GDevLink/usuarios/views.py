cab5fafd757c52d5a1096b872647c5e31cd36b2c
Gabriel Gomez Corredor <gomezga@javeriana.edu.co>
2020-10-21 03:34:26 -0500

    Editar perfil

--- GDevLink/usuarios/views.py
+++ GDevLink/usuarios/views.py
@@ -95,31 +95,66 @@ def perfil(request,nombre_usuario):
         })
 
 def editar(request,nombre_usuario):
-    try:
-        usuario = Usuario.objects.get(username=nombre_usuario)
-        participaciones = {}
-        roles = []
-        generos = []
-        frameworks = []
-        for rol in usuario.roles:
-            roles.append((PosiblesRoles.labels[PosiblesRoles.values.index(rol)]))
-        for genero in usuario.generos:
-            generos.append((PosiblesGeneros.labels[PosiblesGeneros.values.index(genero)]))
-        for framework in usuario.frameworks:
-            frameworks.append((PosiblesFrameworks.labels[PosiblesFrameworks.values.index(framework)]))
-        for participacion in usuario.participaciones.all():
-            roles_p = ""
-            for rol in participacion.roles:
-                roles_p= roles_p + " " + str(PosiblesRoles.labels[PosiblesRoles.values.index(rol)])
-            participaciones[participacion.proyecto.nombre] = roles_p
-        return render(request, "usuarios/editar.html", {
-            "usuario": usuario,
-            "participaciones": participaciones,
-            "roles": roles,
-            "generos": generos,
-            "frameworks": frameworks
-        })
-    except Usuario.DoesNotExist:
-        return render(request, "main/error.html", {
-            "mensaje": "Usuario no encontrado."
-        })
+    if request.method == "POST":
+        roles = request.POST.getlist('roles')
+        generos = request.POST.getlist('generos')
+        frameworks = request.POST.getlist('frameworks')
+
+
+        for f in roles:
+            print(f)
+
+        # Ensure password matches confirmation
+        #password = request.POST["password"]
+        #confirmation = request.POST["confirmation"]
+        #if password != confirmation:
+        #    return render(request, "usuarios/registrar.html", {
+        #        "message": "Las contraseñas no coinciden."
+        #    })
+
+        # Attempt to create new user
+        try:
+            user = Usuario.objects.get(username=nombre_usuario)
+            user.roles = roles
+            user.generos = generos
+            user.frameworks = frameworks
+            user.save()
+        except IntegrityError as e:
+            print(e)
+            return render(request, "usuarios/editar.html", {
+                "message": "Correo o nombre de usuario ya registrado."
+            })
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        try:
+            usuario = Usuario.objects.get(username=nombre_usuario)
+            participaciones = {}
+            roles = []
+            generos = []
+            frameworks = []
+            for rol in usuario.roles:
+                roles.append((PosiblesRoles.labels[PosiblesRoles.values.index(rol)]))
+            for genero in usuario.generos:
+                generos.append((PosiblesGeneros.labels[PosiblesGeneros.values.index(genero)]))
+            for framework in usuario.frameworks:
+                frameworks.append((PosiblesFrameworks.labels[PosiblesFrameworks.values.index(framework)]))
+            for participacion in usuario.participaciones.all():
+                roles_p = ""
+                for rol in participacion.roles:
+                    roles_p= roles_p + " " + str(PosiblesRoles.labels[PosiblesRoles.values.index(rol)])
+                participaciones[participacion.proyecto.nombre] = roles_p
+            return render(request, "usuarios/editar.html", {
+                "usuario": usuario,
+                "participaciones": participaciones,
+                "roles": roles,
+                "posiblesRoles": PosiblesRoles,
+                "generos": generos,
+                "posiblesGeneros": PosiblesGeneros,
+                "frameworks": frameworks,
+                "posiblesFrameworks": PosiblesFrameworks
+            })
+        except Usuario.DoesNotExist:
+            return render(request, "main/error.html", {
+                "mensaje": "Usuario no encontrado."
+            })
