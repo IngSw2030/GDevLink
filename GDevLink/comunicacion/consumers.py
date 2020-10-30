7016d8bcfd91190753dfd2a574bffa5087594b81
SantiagoCaroprese <66383253+SantiagoCaroprese@users.noreply.github.com>
2020-10-29 20:00:43 -0500

    Chat funcionando

--- GDevLink/comunicacion/consumers.py
+++ GDevLink/comunicacion/consumers.py
@@ -25,7 +25,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
-
+     
         # Send message to room group
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -38,3 +38,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
     # Receive message from room group
     async def chat_message(self, event):
         message = event['message']
+
+        await self.send(text_data=json.dumps({
+            'message': message
+        }))
