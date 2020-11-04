0aad16087214cccd93d6843917a668c11ee8aba5
Reshi01 <danieldanih97@gmail.com>
2020-11-03 19:26:21 -0500

    Responder a pregunta completo

--- GDevLink/preguntas/urls.py
+++ GDevLink/preguntas/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path("preguntas",views.preguntas,name="preguntas"),
     path("crearPregunta",views.crearPregunta,name="crearPregunta"),
     path("verPregunta/<int:ids>",views.verPregunta,name="verPregunta"),
+    path("crearRespuesta/<int:ids>",views.crearRespuesta,name="crearRespuesta"),
 ]
--- GDevLink/preguntas/views.py
+++ GDevLink/preguntas/views.py
@@ -40,8 +40,30 @@ def verPregunta(request,ids):
     preguntas=Pregunta.objects.all()
     try:
         pregunta=Pregunta.objects.get(id=ids)
-        respuestas=pregunta.respuestas
-        return render(request,"preguntas/verPregunta.html",{"pregunta":pregunta, "respuestas":respuestas})
+        respuestas=pregunta.respuestas.all()
+        if(respuestas is not None):
+            return render(request,"preguntas/verPregunta.html",{"pregunta":pregunta, "respuestas":respuestas})
+        else:
+            return render(request,"preguntas/verPregunta.html",{"pregunta":pregunta})
     except Pregunta.DoesNotExist:
         return HttpResponseRedirect(reverse("preguntas"))
     return HttpResponseRedirect(reverse("preguntas"))
+
+@login_required(login_url='/usuarios/login')
+def crearRespuesta(request,ids):
+    if request.method == "POST":
+        pregunta=Pregunta.objects.get(id=ids)
+        texto = request.POST['texto']
+    try:
+        respuesta = Respuesta(texto=texto,pregunta=pregunta,autor=request.user)
+        respuesta.save()
+        pregunta=Pregunta.objects.get(id=ids)
+        respuestas=pregunta.respuestas.all()
+        return render(request,"preguntas/verPregunta.html",{"pregunta":pregunta, "respuestas":respuestas})
+    except IntegrityError as e:
+        pregunta=Pregunta.objects.get(id=ids)
+        respuestas=pregunta.respuestas
+        return render(request,"preguntas/verPregunta.html",{"pregunta":pregunta, "respuestas":respuestas})
+    pregunta=Pregunta.objects.get(id=ids)
+    respuestas=pregunta.respuestas
+    return render(request,"preguntas/verPregunta.html",{"pregunta":pregunta, "respuestas":respuestas})
