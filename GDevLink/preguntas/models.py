5868031d07f5334526a14a72da107c6968959689
Reshi01 <danieldanih97@gmail.com>
2020-11-04 10:20:15 -0500

    cambio modelo preguntas

--- GDevLink/preguntas/models.py
+++ GDevLink/preguntas/models.py
@@ -8,12 +8,20 @@ class Pregunta(models.Model):
     texto =  models.CharField(max_length=1000, blank=True,null=True)
     autor = models.ForeignKey('usuarios.Usuario',on_delete=models.CASCADE,related_name='preguntas',blank=False,null=False)
     mejorRespuesta = models.ForeignKey('Respuesta',on_delete=models.CASCADE,related_name='mejorRespuesta',blank=True,null=True)
-    puntosPositivos =ArrayField(models.ForeignKey('usuarios.Usuario',on_delete=models.CASCADE,related_name='preguntasPuntosPositivos',blank=True),null=True)
-    puntosNegativos = ArrayField(models.ForeignKey('usuarios.Usuario',on_delete=models.CASCADE,related_name='preguntasPuntosNegativos',blank=True),null=True)
+    puntosPositivos =models.ManyToManyField('usuarios.Usuario', blank=True, related_name="preguntasPuntosPositivos")
+    puntosNegativos =models.ManyToManyField('usuarios.Usuario', blank=True, related_name="preguntasPuntosNegativos")
+    def puntos_positivos(self):
+        return self.puntosPositivos.all().count()
+    def puntos_negativos(self):
+        return self.puntosNegativos.all().count()
 
 class Respuesta(models.Model):
     pregunta = models.ForeignKey('Pregunta',on_delete=models.CASCADE,related_name='respuestas',blank=False,null=False)
     texto =  models.CharField(max_length=1000, blank=True,null=True)
     autor = models.ForeignKey('usuarios.Usuario',on_delete=models.CASCADE,related_name='respuestas',blank=False,null=False)
-    puntosPositivos = ArrayField(models.ForeignKey('usuarios.Usuario',on_delete=models.CASCADE,related_name='respuestasPuntosPositivos'),blank=True,null=True)
-    puntosNegativos = ArrayField(models.ForeignKey('usuarios.Usuario',on_delete=models.CASCADE,related_name='resouestasPuntosNegativos'),blank=True,null=True)
+    puntosPositivos =models.ManyToManyField('usuarios.Usuario', blank=True, related_name="respuestasPuntosPositivos")
+    puntosNegativos =models.ManyToManyField('usuarios.Usuario', blank=True, related_name="resouestasPuntosNegativos")
+    def puntos_positivos(self):
+        return self.puntosPositivos.all().count()
+    def puntos_negativos(self):
+        return self.puntosNegativos.all().count()
