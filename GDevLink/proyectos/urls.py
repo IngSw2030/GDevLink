8d137eeb21f843492f5132e1ac1eff66ea235fab
JuanCarlosSuarezJaimes <juan.suarezj@javeriana.edu.co>
2020-11-24 10:08:08 -0500

    Correción de errores.

--- GDevLink/proyectos/urls.py
+++ GDevLink/proyectos/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path("", views.explorarProyectos, name="explorarProyectos"),
     path("crearProyecto", views.crearProyecto,name="crearProyecto"),
     path("MisProyectos",views.proyectosUsuario,name="MisProyectos"),
     path("proyecto/<str:nombre>",views.proyecto,name="proyecto"),
@@ -11,7 +12,5 @@ urlpatterns = [
     path("seguir/<str:nombre>", views.seguir, name="seguir"),
     path("promover/<str:nombre>", views.promover, name="promover"),
     path("eliminar/<str:nombre>", views.eliminar, name="eliminar"),
-    path("revocar/<str:nombre>", views.revocar, name="revocar"),
-    path("explorarProyectos", views.explorarProyectos, name="explorarProyectos"),
-    
+    path("revocar/<str:nombre>", views.revocar, name="revocar") 
 ]
